42e57e8c5aa5deec55d2d97d95352b4c3e50b270
nicholaKim <86nichola@gmail.com>
2021-03-24 12:31:37 +0900

    tmp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
+import React from 'react';
+import './App.css';
 import Hello from './Hello.js';
 
 function App() {
+  const name = 'react';
+  const style = {
+    backgroundColor: 'red',
+    color: 'aqua',
+    fontSize: '24',
+    padding:'1rem'
+  }
   return (
-    <div>
-      <Hello />
-    </div>
+    <>
+      <Hello name="react" color="red" />
+      <Hello color="pink" />
+    </>
   );
 }
 
--- src/Hello.js
+++ src/Hello.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-function Hello (){
-    return <div>Hi~!</div>
+function Hello({ name, color }) {
+    console.log(color);
+    debugger;
+    return <div style={{ color }}>Hi~!{name}</div>
+}
+
+Hello.defaultProps = {
+    name: '이름없음'
 }
 
 export default Hello;
